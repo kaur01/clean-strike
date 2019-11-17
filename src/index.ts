c2dd90b7d43a14b42a15b914554b3f9975a960dc
Harpreet <harpreet@finzy.com>
2019-11-18 01:55:45 +0530

    Initialised index.ts.

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {Game} from "./models/Game";
+
+console.log("Choose the type of game you want to play?");
+console.log("1. Player1 vs Player2");
+console.log("2. StrikeBot1 vs StrikeBot2");
+const game = new Game();
