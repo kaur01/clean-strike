4b43343a9f9f9e61625033c7cce63dcf5d009580
Harpreet <harpreet@finzy.com>
2019-11-18 20:30:20 +0530

    Added Selection enum.

---
+++ src/models/Selection.ts
@@ -0,0 +1,8 @@
+export enum Selection {
+    Strike = 'strike',
+    MultiStrike = 'multi_strike',
+    RedStrike = 'red_strike',
+    StrikerStrike = 'striker_strike',
+    DefunctCoin = 'defunct_coin',
+    None = 'none'
+}
